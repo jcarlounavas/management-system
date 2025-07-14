4f3c085b5c1c9fb7f765b7a1bcd0fb82819a5e79
navs <jcarlounavas@gmail.com>
2025-07-15 07:04:39 +0800

    Set up

---
+++ gcthms/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import InputAreas from './components/inputArea.jsx';
+
+function App() {
+  return (
+    <div className="App">
+      <main>
+        <InputAreas />
+      </main>
+    </div>
+  );
+}
+
+export default App;
---
+++ gcthms/src/components/inputArea.jsx
@@ -0,0 +1,23 @@
+
+
+const input = () => {
+  return (
+    <div className="input-area">
+
+      <form>
+          <div className="form-group">
+            <input type='file' accept='.pdf' id='files' />
+
+            <button type="submit">Submit</button>
+          </div>   
+
+      </form>
+          
+      
+
+
+    </div>
+  )
+}
+
+export default input;
